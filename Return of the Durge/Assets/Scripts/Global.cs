848158c45688e78aa39980308226f8c4cc17f237
actuallyIgnoble <jakeselv@gmail.com>
2021-11-04 14:07:20 -0400

    Pushing For the Boys(tm)
    Added some stuff to the game manager, created a 2D array to start working on the map room codes

--- Return of the Durge/Assets/Scripts/Global.cs
+++ Return of the Durge/Assets/Scripts/Global.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class Global : ScriptableObject
 {
     public int numRooms;
+
 }
--- Return of the Durge/Assets/Scripts/gameManager.cs
+++ Return of the Durge/Assets/Scripts/gameManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int vertical, horizontal;
+
+    string[,] roomMap = new string[7, 7];
+    public int rowsGenerated;
+    public int colsGenerated;
     void Start()
     {
-        
+        roomMap[4, 4] = "P:" + "S";
+        vertical = (int)Camera.main.orthographicSize;
+        horizontal = vertical * (Screen.width / Screen.height);
+        for (int row = 0; row < 7; row++)
+        {
+            for (int col = 0; col < 7; col++)
+            {
+                Debug.Log("F");
+                roomMap[row, col] = "F";
+            }
+            Debug.Log("\n");
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
