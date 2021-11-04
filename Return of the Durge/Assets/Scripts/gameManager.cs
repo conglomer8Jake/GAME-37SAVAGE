ee4d85f1f35915f76f16a6590b2b3b06d91deb91
actuallyIgnoble <jakeselv@gmail.com>
2021-11-04 15:46:16 -0400

    preliminary room gen stuff For the Boys(tm)
    Added in individual room spawners that identify where they are in the room and where they lead, destroy on collision. Implemented "+" junction room to spawn seamlessly with starting room. Began to work with 2D array and trying to update player position.

--- Return of the Durge/Assets/Scripts/gameManager.cs
+++ Return of the Durge/Assets/Scripts/gameManager.cs
@@ -9,9 +9,11 @@ public class gameManager : MonoBehaviour
     string[,] roomMap = new string[7, 7];
     public int rowsGenerated;
     public int colsGenerated;
+    public int playerPosX;
+    public int playerPosY;
     void Start()
     {
-        roomMap[4, 4] = "P:" + "S";
+        roomMap[4, 4] = "P";
         vertical = (int)Camera.main.orthographicSize;
         horizontal = vertical * (Screen.width / Screen.height);
         for (int row = 0; row < 7; row++)
@@ -24,8 +26,23 @@ public class gameManager : MonoBehaviour
             Debug.Log("\n");
         }
     }
-    void Update()
+    public void updateMap()
     {
-
+        Debug.Log("updateMap");
+        for (int row = 0; row < 7; row++)
+        {
+            Debug.Log("row");
+            for (int col = 0; col < 7; col++)
+            {
+                Debug.Log("col");
+                if (roomMap[row,col].Contains("P"))
+                {
+                    Debug.Log("containCheck");
+                    playerPosX = col;
+                    playerPosY = row;
+                    return;
+                }
+            }
+        }
     }
 }
--- Return of the Durge/Assets/Scripts/roomGenerationScript.cs
+++ Return of the Durge/Assets/Scripts/roomGenerationScript.cs
@@ -4,43 +4,69 @@ using UnityEngine;
 
 public class roomGenerationScript : MonoBehaviour
 {
+    public gameManager gM;
     public Global vH;
     public listOfRooms lOR;
     public GameObject roomCenter;
+    public GameObject roomGenerated;
     public int[] numDoorsRemaining;
     public int randNum;
     public float displacementY;
     public float displacementX;
+    public float verticalDiff;
+    public float horizontalDiff;
     public string direction;
     void Start()
     {
+        gM = GameObject.FindObjectOfType<gameManager>();
         roomCenter = GameObject.FindGameObjectWithTag("center");
         displacementY = this.gameObject.transform.position.y - roomCenter.transform.position.y;
         displacementX = this.gameObject.transform.position.x - roomCenter.transform.position.x;
-        if (displacementX > 0)
+        if (displacementX != 0)
         {
-            direction = "right";
+            if (displacementX > 0)
+            {
+                direction = "right";
+                verticalDiff = 0.0f;
+                horizontalDiff = 34.0569f;
+            }
+            if (displacementX < 0)
+            {
+                direction = "left";
+                verticalDiff = 0.0f;
+                horizontalDiff = -32.944f;
+            }
         }
-        if (displacementX < 0)
+        if (displacementY != 0)
         {
-            direction = "left";
-        }
-        if (displacementY > 0)
-        {
-            direction = "up";
-        }
-        if (displacementY < 0)
-        {
-            direction = "down";
+            if (displacementY > 0)
+            {
+                direction = "up";
+                verticalDiff = 28.27f;
+                horizontalDiff = 0.0f;
+            }
+            if (displacementY < 0)
+            {
+                direction = "down";
+                verticalDiff = -27.27f;
+                horizontalDiff = 0.0f;
+            }
         }
     }
     public void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            randNum = Random.Range(0, 3);
-            Instantiate(lOR.rooms[randNum], this.gameObject.transform.position, Quaternion.identity);
+            randNum = Random.Range(10, 10);
+            Instantiate(lOR.rooms[randNum], new Vector2(this.gameObject.transform.position.x+horizontalDiff, this.gameObject.transform.position.y+verticalDiff), Quaternion.identity);
+            if (direction == "right")
+            {
+                Debug.Log("right");
+                gM.updateMap();
+            }
+            
             vH.numRooms++;
+            Destroy(this.gameObject);
         }
     }
 }
