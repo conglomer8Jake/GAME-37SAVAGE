7670dbaca06c595e38c142a35896acd2923af69c
Elijah Anderson <ejanderson00@gmail.com>
2021-11-12 01:29:00 -0500

    Tested bullets and finished thing
    Hell yeah

---
+++ Return of the Durge/Assets/Scripts/SpinScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour {
+
+    float CurrentRotation;
+
+    public float RotationSpeed = 180.0f;
+
+	// Use this for initialization
+	void Start () {
+        CurrentRotation = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentRotation += RotationSpeed * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Euler(
+            0.0f,
+            0.0f,
+            CurrentRotation
+            );
+	}
+}
