1e3a74458f81f3f5aa7b7a2d5ac7737074e0202b
actuallyIgnoble <jakeselv@gmail.com>
2021-11-16 16:03:14 -0500

    ninjaDurgy work For the Boys(tm)
    >Modified the animator to have an "isShooting" boolean
    >Modified ninjaDurgy script to transition between states based on the "isShooting" boolean
    >Added a disappear() function to disable the sprite renderer if the ninjaDurgy is not shooting, needs timing help

---
+++ Return of the Durge/Assets/Scripts/Durgy Scripts/ninjaDurgy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ninjaDurgy : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject Ninja;
+    public ninjaDurgy ninjaScript;
+    public ParticleSystem ninjaSystem;
+
+    public float shootTimer = 6.0f;
+    public bool timerOn;
+    void Start()
+    {
+        ninjaScript = GameObject.FindObjectOfType<ninjaDurgy>();
+        Ninja = ninjaScript.gameObject;
+        ninjaSystem = Ninja.GetComponentInChildren<ParticleSystem>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+    public void Update()
+    {
+        if (ninjaSystem.GetComponentInChildren<ParticleSystem>().isStopped)
+        {
+            shadowWalk();
+            timerOn = true;
+        }
+        if (timerOn)
+        {
+            shootTimer -= 1 * Time.deltaTime;
+        }
+        if (shootTimer <= 0)
+        {
+            shoot();
+            timerOn = false;
+            shootTimer = 6.0f;
+        }
+        if (anim.GetBool("isShooting"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+    public void shadowWalk()
+    {
+        anim.SetBool("isShooting", false);
+        disappear();
+    }
+    public void shoot()
+    {
+        ninjaSystem.GetComponentInChildren<ParticleSystem>().Play();
+        anim.SetBool("isShooting", true);
+    }
+    public void disappear()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
