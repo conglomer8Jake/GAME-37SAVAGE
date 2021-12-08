76257a15e486f0e1a4a216e1fc8eb8f5b9b55d6d
SneezeJPG <95330386+SneezeJPG@users.noreply.github.com>
2021-12-08 18:49:51 -0500

    New level 3 music
    added level 3 music for boss or main level, either is fine

---
+++ RobotGame231/Assets/PlayerController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float jumpDelayTime = 0.2f;
+    float respawnTime = 2.0f;
+    public float jumpSpeed = 1.0f;
+    float gravity = 20.0f;
+    public float jumpAnimSpeed = 1.0f;
+    public float walkSpeed = 2.0f;
+    public float walkAnimSpeed = 1.0f;
+    public float runSpeed = 6.0f;
+    public float runAnimSpeed = 1.25f;
+
+    private float rotateSpeed = 150.0f;
+    bool grounded = false;
+    Vector3 moveDirection = Vector3.zero;
+    bool isWalking = true;
+    string moveStatus = "idle";
+    float xSpeed = 250.0f;
+    float ySpeed = 120.0f;
+    float yMin = -40.0f;
+    float yMax = 80.0f;
+    float x = 0.0f;
+    float y = 0.0f;
+    bool jumpMove;
+    bool canJump;
+    bool dead;
+    Vector3 startPos;
+    Quaternion startRot;
+
+    public AnimationClip idleAnim;
+    public AnimationClip walkAnim;
+    public AnimationClip runAnim;
+    public AnimationClip jumpAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animation>()[jumpAnim.name].layer = 10;
+        canJump = true;
+        startPos = this.transform.position;
+        startRot = this.transform.rotation;
+    }
+
+    void Update()
+    {
+        if (dead == false)
+        {
+            GetComponent<Animation>()[walkAnim.name].speed = walkAnimSpeed;
+            GetComponent<Animation>()[runAnim.name].speed = runAnimSpeed;
+            //Only allow movement and jumps while grounded
+            if (grounded)
+            {
+                moveDirection = new Vector3((Input.GetMouseButton(1) ? Input.GetAxis("Horizontal") : 0), 0, Input.GetAxis("Vertical"));
+                
+                moveDirection *= isWalking ? walkSpeed : runSpeed;
+                
+                moveStatus = "idle";
+                if(moveDirection != Vector3.zero)
+                {
+                    moveStatus = !isWalking ? "walking" : "running";
+                    if (isWalking)
+                    {
+                        //invoke walk animation
+                        GetComponent<Animation>().Play(walkAnim.name);
+                    }
+                    else
+                    {
+                        //call Run animation
+                        GetComponent<Animation>().CrossFade(runAnim.name);
+                    }
+                }
+                else
+                {
+                    //call idle animation
+                    GetComponent<Animation>().CrossFade(idleAnim.name);
+                }
+                
+                //Jump
+                if(Input.GetKeyDown(KeyCode.Space) && canJump == true)
+                {
+                    canJump = false;
+                    AnimationState jmp = GetComponent<Animation>().CrossFadeQueued(jumpAnim.name, 0.1f, QueueMode.PlayNow);
+                    jmp.speed = jumpAnimSpeed;
+                    StartCoroutine(JumpDelay());
+                }
+                if(jumpMove == true)
+                {
+                    moveDirection.y = 2 * jumpSpeed;
+                    moveDirection += transform.forward * jumpSpeed;
+                    jumpMove = false;
+                    canJump = true;
+                }
+            } //End Grounded
+            // Allow turning at anytime
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
+
+            if(Input.GetKeyDown("left shift") || Input.GetKeyDown("right shift"))
+            {//toggle walk/run
+                isWalking = !isWalking;
+            }
+            moveDirection.y -= 10 * gravity * Time.deltaTime;
+            //Move Controller
+            CharacterController controller = GetComponent<CharacterController>();
+            var flags = controller.Move(moveDirection * Time.deltaTime);
+            grounded = (flags & CollisionFlags.Below) != 0;
+
+
+        }
+    }
+
+    IEnumerator JumpDelay()
+    {
+        yield return new WaitForSeconds(jumpDelayTime);
+        jumpMove = true;
+    }
+
+}
---
+++ RobotGame231/Assets/cameraScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float targetHeight = 2.0f;
+    public float distance = 2.8f;
+    public float maxDistance = 10;
+    public float minDistance = 0.5f;
+    float xSpeed = 250.0f;
+    float ySpeed = 120.0f;
+    float yMinLimit = -40.0f;
+    float yMaxLimit = 80.0f;
+    public int zoomRate = 20;
+    float rotationDampening = 3.0f;
+    float x = 0.0f;
+    float y = 0.0f;
+    bool isTalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        //Makes the rigidBody not change rotation
+        if (GetComponent<Rigidbody>())
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if (!target)
+        {
+            return;
+        }
+        if (Input.GetMouseButton(0) || (Input.GetMouseButton(1)))
+        {
+            x += Input.GetAxis("Mouse X") * ySpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * xSpeed * 0.02f;
+
+        }
+        else if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0)
+        {
+            var targetRotationAngle = target.eulerAngles.y;
+            var currentRotationAngle = transform.eulerAngles.y;
+            x = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, rotationDampening * Time.deltaTime);
+
+        }
+
+        distance -= (Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime) * zoomRate * Mathf.Abs(distance);
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+        //Rotate Camera
+        Quaternion rotation = Quaternion.Euler(x, y, 0);
+        transform.rotation = rotation;
+
+        //Position Camera
+        var position = target.position - (rotation * Vector3.forward * distance + new Vector3(0, -targetHeight, 0));
+        transform.position = position;
+
+      /*  //Is View Blocked?
+        RaycastHit hit;
+        Vector3 trueTargetPosition = target.transform.position - new Vector3(0, -targetHeight, 0);
+        // Cast Line to Check
+        if (Physics.Linecast(trueTargetPosition, transform.position, hit))
+        {
+            var tempDist = Vector3.Distance(trueTargetPosition, hit.point) -0.28
+        }*/
+    }
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+        {
+            angle += 360;
+
+        }
+        if(angle > 360)
+        {
+            angle -= 360;
+         }
+        return Mathf.Clamp(angle, min, max);
+    }
+}
+
