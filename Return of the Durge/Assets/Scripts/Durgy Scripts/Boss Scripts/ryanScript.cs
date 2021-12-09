b33a1c0a58fdcfda30f7254807ea9463b0992f58
Elijah Anderson <ejanderson00@gmail.com>
2021-12-09 14:53:46 -0500

    ELIRYAN BOSS PREFAB TWEAK

--- Return of the Durge/Assets/Scripts/Durgy Scripts/Boss Scripts/ryanScript.cs
+++ Return of the Durge/Assets/Scripts/Durgy Scripts/Boss Scripts/ryanScript.cs
@@ -82,13 +82,12 @@ public class ryanScript : MonoBehaviour
     public void dashAttack()
     {
         Anim.SetBool("DashAttacking", true);
-        ForwardThrust(2.3f);
+        //ForwardThrust(2.3f);
         Invoke("finish", 1.0f);
     }
     public void finish()
     {
         this.gameObject.GetComponent<FlockerScript>().SpeedPerSecond = 4.0f;
-        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
         Anim.SetBool("Finished", true);
         Anim.SetBool("SlashStorm", false);
         Anim.SetBool("DashAttacking", false);
